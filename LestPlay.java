f3b9fdaae12627d707440a4c537727db3e2a546b
akash-geek <nakka.akash@gmail.com>
2020-03-28 15:13:11 +0530

    no chnages

--- LestPlay.java
+++ LestPlay.java
@@ -13,30 +13,33 @@ public class LestPlay {
 
 			System.out.println("Hi Please enter option for operations 1.print inorder 2. BFS 3.Compare 4.get Height 5. exit");
 			int cond = sc.nextInt();
-			while(true)
-			switch(cond){
-			case 1:top.traverseInOrder(root);
-			break;
-			case 2:top.printTreeBFS(root);
-			break;
-			case 3:Node root2 = top.constructTree();
-			if(top.compareTwoBinaryTrees(root, root2)){
-				System.out.println("both are same");
-				System.out.println("Tree 1");
-				top.printTreeBFS(root);
-				System.out.println("Tree 2");
-				top.printTreeBFS(root2);
-			}
-			else{ System.out.println("both are different");
-			System.out.println("Tree 1");
-			top.printTreeBFS(root);
-			System.out.println("\nTree 2");
-			top.printTreeBFS(root2);
-			}
-			break;
-			case 4: System.out.println(top.getHeight(root));
-			break
-			case 5: exit(0);
+			while(true){
+				switch(cond){
+					case 1:top.traverseInOrder(root);
+						break;
+					case 2:top.printTreeBFS(root);
+						break;
+					case 3:
+						Node root2 = top.constructTree();
+						if(top.compareTwoBinaryTrees(root, root2)){
+							System.out.println("both are same");
+							System.out.println("Tree 1");
+							top.printTreeBFS(root);
+							System.out.println("Tree 2");
+							top.printTreeBFS(root2);
+						}
+						else{ 
+							System.out.println("both are different");
+							System.out.println("Tree 1");
+							top.printTreeBFS(root);
+							System.out.println("\nTree 2");
+							top.printTreeBFS(root2);
+						}
+						break;
+					case 4: System.out.println(top.getHeight(root));
+						break
+					case 5: exit(0);
+				}
 			}
 		}
 }

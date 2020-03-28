6c5ebe6bca578ea0375fc1be422d3a1eb97b1f6d
akash-geek <nakka.akash@gmail.com>
2020-03-28 14:32:48 +0530

    updated compare binary trees

--- LestPlay.java
+++ LestPlay.java
@@ -5,19 +5,30 @@ import java.util.Scanner;
 public class LestPlay {
 
 	public static void main(String args[]){
-		TreeOps top = new TreeOps();
-		Node root = top.constructTree();
-		int order;
-		@SuppressWarnings("resource")
-		Scanner sc  = new Scanner(System.in);
-		System.out.println("Please ender Order you want 1.PreOrder 2.PostOrder 3.Inorder");
-		order = sc.nextInt();
-		while(order>0){
-			
-		top.printTree(root, order);
-		System.out.println("Please ender -1 to stop the execution");
-		order = sc.nextInt();
-		}
+		Scanner sc = new Scanner(system.in);
+		//System.out.println("Hi Please enter number of threes you want to crate");
 		
-	}
+	
+			TreeOps top = new TreeOps();
+			Node root = top.constructTree();	
+
+			System.out.println("Hi Please enter option for operations 1.print inorder 2. BFS 3.Compare ");
+			int cond = sc.nextInt();
+			switch(cond){
+			case 1:top.traverseInOrder(root);
+			case 2:top.printTreeBFS(root);
+			case 3:Node root2 = top.constructTree();
+			if(compareTwoBinaryTrees(root, root2)){
+				System.out.println("both are same");
+			}
+			else System.out.println("both are different");
+			
+			}
+
+		
+		System.out.println(" \n BFS");
+		
+	 top.printTreeBFS(root);
+		}
+
 }
